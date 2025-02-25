076f4e2e5dc4c9ca5b50d03e3a20100a63514a5b
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-02-25 11:49:28 +0600

    added toggle feature

---
+++ script/toggle.js
@@ -0,0 +1,13 @@
+document.getElementById("cash-out").style.display = "none";
+
+// click add-money button
+document.getElementById("add-money-box").addEventListener("click", function () {
+  document.getElementById("cash-out").style.display = "none";
+  document.getElementById("add-money").style.display = "block";
+});
+
+// click cash-out button
+document.getElementById("cash-out-box").addEventListener("click", function () {
+  document.getElementById("add-money").style.display = "none";
+  document.getElementById("cash-out").style.display = "block";
+});
